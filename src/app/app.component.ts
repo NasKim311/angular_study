85ef871214dc0794e710422f131eb9e80bf9fee2
kimnamsu <kimna0311@naver.com>
2023-03-21 18:57:34 +0900

    2commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'firstStudy';
+
+  constructor() {
+    // -> 최초 단 1번만 동작하는 함수
+    console.log('동작');
+  }
 }
