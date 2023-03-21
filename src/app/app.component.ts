6b5ca06bdb632918278a8a84dbc8c6267feab2cc
kimnamsu <kimna0311@naver.com>
2023-03-21 21:06:27 +0900

    4강 완료

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 
+const array: Array<string> = ['data0', 'data1', 'data2'];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstStudy';
+  title: string = 'firstStudy';
+
+  showArray: Array<string>;
 
   constructor() {
     // -> 최초 단 1번만 동작하는 함수
-    console.log('동작');
+    this.showArray = array;
   }
 
   public clickAfterPrint(): void {
-    console.log('출력');
+    this._innerFunc();
+    console.log(array);
+    console.log(this.title);
+  }
+
+  private _innerFunc() {
+    array.push('data' + array.length);
   }
 }
