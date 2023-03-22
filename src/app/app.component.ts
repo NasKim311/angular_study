e6b8e8cc1bc2e4ffa932ccd6a6b26a97b1f270f7
kimnamsu <kimna0311@naver.com>
2023-03-22 09:57:39 +0900

    5강 완료

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 
 const array: Array<string> = ['data0', 'data1', 'data2'];
 
+// key, value 형식의 내용
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
+// 각종 기능들
 export class AppComponent {
   title: string = 'firstStudy';
 
   showArray: Array<string>;
 
+  // 최초 단 1번만 동작하는 함수
   constructor() {
-    // -> 최초 단 1번만 동작하는 함수
     this.showArray = array;
   }
 
